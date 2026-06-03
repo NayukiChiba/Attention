07989d68459b2c34a3f6dfb9ea1600f3265358e6
NayukiMeko <MekoNayuki@outlook.com>
2026-06-03 21:25:59 +0800

    feat(download):  新增解压zip文件的函数

--- src/data/download.py
+++ src/data/download.py
@@ -6,20 +6,22 @@ THUCNews数据集下载脚本
 1. 从清华 NLP 官方源下载 THUCNews 数据集
 2. 自动解压
 """
-import zipfile
-import requests
-from pathlib import Path
-from tqdm import tqdm
-from config import paths
 
+import zipfile
+from pathlib import Path
+
+import requests
+from tqdm import tqdm
+
+from config import paths
 
 THUCNEWS_URL = [
     "http://thuctc.thunlp.org/THUCNews.zip",
-    "https://github.com/skdjfla/THUCNews/archive/refs/heads/master.zip"
+    "https://github.com/skdjfla/THUCNews/archive/refs/heads/master.zip",
 ]
 
-RAW_ZIP_PATH = paths.RAW_DATASETS_DIR / 'THUCNews.zip'
-EXTRACTED_DIR = paths.RAW_DATASETS_DIR / 'THUCNews'
+RAW_ZIP_PATH = paths.RAW_DATASETS_DIR / "THUCNews.zip"
+EXTRACTED_DIR = paths.RAW_DATASETS_DIR / "THUCNews"
 
 
 def download_file(url: str, save_path: Path) -> None:
@@ -34,8 +36,8 @@ def download_file(url: str, save_path: Path) -> None:
     response = requests.get(url, stream=True, timeout=30)
     response.raise_for_status()  # 检查请求是否成功
 
-    total_size = int(response.headers.get('content-length', 0))
-    block_size = 1024*1024  # 限制下载速度为1MB/s
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024 * 1024  # 限制下载速度为1MB/s
     # write + binary模式打开文件
     with save_path.open("wb") as file:
         # unit="B"表示以字节为单位显示,unit_scale=True表示自动调整单位(KB, MB, GB等)
@@ -44,7 +46,17 @@ def download_file(url: str, save_path: Path) -> None:
                 file.write(chunk)
                 pbar.update(len(chunk))
 
-        
     print(f"数据集下载完成: {save_path}")
 
 
+def extract_zip(zip_path: Path, extract_dir: Path) -> None:
+    """
+    解压 zip 文件
+    Args:
+        zip_path(Path): zip 文件路径
+        extract_dir(Path): 解压后文件保存目录
+    """
+    print(f"正在解压数据集: {zip_path}")
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_dir)
+    print(f"数据集解压完成: {extract_dir}")
